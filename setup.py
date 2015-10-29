22e623ff3acfa7c24fe041086ba989ee869432a4
Sachin Philip Mathew <me@imsach.in>
2015-10-29 13:40:31 +0530

    initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='oneconsole',
+      version='0.1',
+      description='Python package for Oneconsole',
+      url='http://github.com/sachinvettithanam/oneconsole',
+      author='Sachin Philip Mathew',
+      author_email='sachinvettithanam@gmail.com',
+      license='MIT',
+      packages=['oneconsole'],
+      zip_safe=False)
