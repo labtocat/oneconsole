3a9eaf913beda13fda29911b7e60fa15a32903d1
Sachin Philip Mathew <me@imsach.in>
2015-10-30 19:29:37 +0530

    inititated apis

--- oneconsole/__init__.py
+++ oneconsole/__init__.py
@@ -1,3 +1,47 @@
-def joke():
-    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
-            u'Beiherhund das Oder die Flipperwaldt gersput.')
+import requests
+
+
+def login(username, password):
+	r = requests.post("http://oneconsole.us/api/v1/user/login/",
+					data = {'username' : username, "password":password})
+	elem = r.json()
+	return elem
+
+
+def logout(username):
+	pass
+
+
+def profile(token):
+	r = requests.get("http://oneconsole.us/api/v1/profile/?format=json&auth="+token)
+	get_json = r.json()
+	elem = get_json['objects'][0]
+	# title = str(elem['title'])
+	# json = {"title":title}
+	return elem
+
+
+def addworklog(token, worklog):
+	pass
+
+
+def ticket(token, id):
+	r = requests.get("http://oneconsole.us/api/v1/ticket/?format=json&auth="+token+"&tid="+id)
+	get_json = r.json()
+	elem = get_json['objects'][0]
+	return elem
+
+
+
+def notifications(token):
+	r = requests.get("http://oneconsole.us/api/v1/notification/?format=json&&auth="+token)
+	get_json = r.json()
+	elem = get_json['objects']
+	return elem
+
+
+def alltickets(token):
+	r = requests.get("http://oneconsole.us/api/v1/yourticket/?format=json&&auth="+token)
+	get_json = r.json()
+	elem = get_json['objects']
+	return elem
--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(name='oneconsole',
-      version='0.1',
+      version='0.1.3',
       description='Python package for Oneconsole',
-      url='http://github.com/sachinvettithanam/oneconsole',
+      # url='http://github.com/sachinvettithanam/oneconsole',
       author='Sachin Philip Mathew',
       author_email='sachinvettithanam@gmail.com',
       license='MIT',
       packages=['oneconsole'],
+      install_requires=[
+          'requests',
+      ],
       zip_safe=False)
